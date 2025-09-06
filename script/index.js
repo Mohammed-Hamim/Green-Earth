7b07a4ea16360cf0cccc2a1105fdd0f16292183d
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-09-07 04:32:30 +0600

    active color is added to the category name

--- script/index.js
+++ script/index.js
@@ -17,10 +17,11 @@ const displayCategories = (categories) => {
     categories.forEach(category => {
 
         // create Element for displaying category name 
-        const categoryName = document.createElement('button');
+        const categoryName = document.createElement('div');
 
-        categoryName.innerText = `${category.category_name}`;
-        categoryName.classList.add("py-2", "mb-2", "w-full",);
+        categoryName.innerHTML = `
+        <button id="button-${category.id}" onclick="loadByCategory(${category.id})" class="category-btn py-2 mb-2 w-full">${category.category_name}</button>
+        `;
 
         // append to the categoryContainer
         categoryContainer.appendChild(categoryName);
@@ -68,3 +69,64 @@ const displayPlants = (plants) => {
 }
 loadPlants();
 /*************Functionality of cart container ends here****************/
+
+
+/*************Functionality of showing plant card according to their category starts here****************/
+// function for loading plants by category
+const loadByCategory = (id) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            removeActive()// remove active class
+
+            // select clicked button
+            const buttonNum = document.querySelector(`#button-${id}`);
+            buttonNum.classList.add("active");// add active class
+            displayByCategory(data.plants)
+        })
+}
+
+// function for displaying plants by category
+const displayByCategory = (plants) => {
+    // get the plat card container and empty it
+    const plantContainer = document.getElementById("plant-container");
+    plantContainer.innerHTML = "";
+    // get every category
+    plants.forEach(plant => {
+
+        // create element
+        const plantCard = document.createElement("div");
+        plantCard.innerHTML = `
+         <div class="bg-white p-4 space-y-4 rounded-lg  ">
+                        <img class="max-h-[190px] w-full rounded-lg " src="${plant.image ? plant.image : "No image found"}" alt="">
+                        <h2 class="text-bold text-xl ">${plant.name ? plant.name : "No name found"}</h2>
+                        <p>${plant.description ? plant.description : "No description found"}</p>
+                        <div class="flex justify-between items-center">
+                            <p class="bg-[#CFF0DC] p-3 rounded-full text-sm">${plant.category ? plant.category : "No category found"}</p>
+                            <p class="text-bold"><i class="fa-solid fa-bangladeshi-taka-sign"></i><span>${plant.price ? plant.price : "Price is not available"}</span></p>
+                        </div>
+                        <button class="bg-[#15803D] btn rounded-full w-full text-white">Add to Cart</button>
+                    </div>
+        `;
+        // append to the plant card container 
+        plantContainer.appendChild(plantCard);
+    })
+
+}
+
+// function for removing active color 
+const removeActive = () => {
+    // get all buttons
+    const categoryButton = document.querySelectorAll(".category-btn");
+
+    // select each button
+    categoryButton.forEach(btn => {
+        btn.classList.remove("active")
+    })
+}
+
+/*************Functionality of showing plant card according to their category ends here****************/
+
+
+
